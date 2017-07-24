0ce526bddeda9e64c2a19105a92f76f2f7e66611
Tim Wheeler <wheelert@stanford.edu>
2017-07-24 11:51:09 -0700

    initial package

---
+++ src/ExprRules.jl
@@ -0,0 +1,249 @@
+__precompile__()
+
+module ExprRules
+
+using TreeView
+using StatsBase
+
+export
+        RuleSet,
+        RuleNode,
+        NodeLoc,
+
+        @ruleset,
+        isterminal,
+        get_executable,
+        sample
+
+"""
+isterminal(rule::Any, types::AbstractVector{Symbol})
+
+Returns true if the rule is terminal, ie does not contain any of the types in the provided vector.
+For example, :(x) is terminal, and :(1+1) is terminal, but :(Real + Real) is typically not.
+"""
+function isterminal(rule::Any, types::AbstractVector{Symbol})
+    if isa(rule, Expr)
+        for arg in rule.args
+            if !isterminal(arg, types)
+                return false
+            end
+        end
+    end
+    return rule ∉ types
+end
+
+"""
+TODO: docs
+- generated via macro
+- maybe have an example of use
+"""
+struct RuleSet
+    rules::Vector{Any}
+    types::Vector{Symbol}
+    isterminal::BitVector
+    bytype::Dict{Symbol,Vector{Int}}
+end
+macro ruleset(ex)
+    rules = Any[]
+    types = Symbol[]
+    bytype = Dict{Symbol,Vector{Int}}()
+    for e in ex.args
+        if e.head == :(=)
+            s = e.args[1] # name of return type
+            r = e.args[2] # expression?
+            push!(rules, r)
+            push!(types, s)
+            bytype[s] = push!(get(bytype, s, Int[]), length(rules))
+        end
+    end
+    alltypes = collect(keys(bytype))
+    is_terminal = [isterminal(rule, alltypes) for rule in rules]
+    return RuleSet(rules, types, is_terminal, bytype)
+end
+
+"""
+    RuleNode
+
+INSERT DOCS HERE
+"""
+struct RuleNode
+    ind::Int # index in ruleset
+    children::Vector{RuleNode}
+end
+RuleNode(ind::Int) = RuleNode(ind, RuleNode[])
+
+"""
+    get_executable(rulenode::RuleNode, ruleset::RuleSet)
+
+INSERT DOCS HERE
+"""
+function _get_executable!(expr::Expr, rulenode::RuleNode, ruleset::RuleSet)
+    j = 0
+    stack = Expr[expr]
+    while !isempty(stack)
+        ex = pop!(stack)
+        for (k,arg) in enumerate(ex.args)
+            if haskey(ruleset.bytype, arg)
+                child = rulenode.children[j+=1]
+                ex.args[k] = deepcopy(ruleset.rules[child.ind])
+                if !ruleset.isterminal[child.ind]
+                    _get_executable!(ex.args[k], child, ruleset)
+                end
+            elseif isa(arg, Expr)
+                push!(stack, arg)
+            end
+        end
+    end
+    return expr
+end
+function get_executable(rulenode::RuleNode, ruleset::RuleSet)
+    root = deepcopy(ruleset.rules[rulenode.ind])
+    if !ruleset.isterminal[rulenode.ind] # not terminal
+        _get_executable!(root, rulenode, ruleset)
+    end
+    return root
+end
+
+Core.eval(rulenode::RuleNode, ruleset::RuleSet) = eval(Main, get_executable(rulenode, ruleset))
+Base.show(io::IO, rulenode::RuleNode) =
+function Base.display(rulenode::RuleNode)
+    root = get_executable(rulenode, ruleset)
+    if isa(root, Expr)
+        walk_tree(root)
+    else
+        root
+    end
+end
+function Base.show(io::IO, ::MIME"text/html", rulenode::RuleNode)
+    if isdefined(Main, :IJulia)
+        # If IJulia is present, display using TreeView
+        display(rulenode)
+    else
+        # not, dump the expression
+        dump(get_executable(rulenode, ruleset))
+    end
+end
+
+"""
+    rand(::Type{RuleNode}, ruleset::RuleSet, typ::Symbol, max_depth::Int=10)
+
+Generates a random RuleNode of return type typ and maximum depth max_depth.
+"""
+function Base.rand(::Type{RuleNode}, ruleset::RuleSet, typ::Symbol, max_depth::Int=10)
+
+    rules = ruleset.bytype[typ]
+    rule_index = max_depth > 1 ?
+        StatsBase.sample([rules[i] for i in 1 : length(rules)]) :
+        StatsBase.sample([rules[i] for i in 1 : length(rules) if ruleset.isterminal[i]])
+
+    rulenode = RuleNode(rule_index)
+
+    if !ruleset.isterminal[rule_index]
+        # add children
+        for arg in ruleset.rules[rule_index].args
+            if haskey(ruleset.bytype, arg)
+                child = rand(RuleNode, ruleset, arg, max_depth-1)
+                push!(rulenode.children, child)
+            end
+        end
+    end
+
+    return rulenode
+end
+
+"""
+    sample(root::RuleNode, typ::Symbol, ruleset::RuleSet)
+
+Selects a uniformly random node from the tree.
+"""
+function StatsBase.sample(root::RuleNode)
+    i = 0
+    selected = root
+    stack = RuleNode[root]
+    while !isempty(stack)
+        node = pop!(stack)
+        i += 1
+        if rand() ≤ 1/i
+            selected = node
+        end
+        append!(stack, node.children)
+    end
+    return selected
+end
+
+"""
+    sample(rulenode::RuleNode, typ::Symbol, ruleset::RuleSet)
+
+Selects a uniformly random node of the given return type, typ.
+"""
+function StatsBase.sample(root::RuleNode, typ::Symbol, ruleset::RuleSet)
+    i = 0
+    selected = root
+    stack = RuleNode[root]
+    while !isempty(stack)
+        node = pop!(stack)
+        if ruleset.types[node.ind] == typ
+            i += 1
+            if rand() ≤ 1/i
+                selected = node
+            end
+        end
+        append!(stack, node.children)
+    end
+    ruleset.types[selected.ind] == typ || error("type $typ not found in RuleNode")
+    return selected
+end
+
+"""
+    NodeLoc
+
+A helper struct that points to a node in the tree via its parent such that the child can be easily
+swapped out.
+"""
+struct NodeLoc
+    parent::RuleNode
+    i::Int
+end
+
+"""
+    insert!(loc::NodeLoc, rulenode::RuleNode)
+
+Replaces the subtree pointed to by loc with the given rulenode.
+"""
+function Base.insert!(root::RuleNode, loc::NodeLoc, rulenode::RuleNode)
+    parent, i = loc.parent, loc.i
+    if loc.i > 0
+        typ = ruleset.types[parent.children[i].ind]
+        parent.children[i] = rulenode
+    else
+        root = rulenode
+    end
+    return root
+end
+
+"""
+    sample(::Type{NodeLoc}, root::RuleNode)
+
+Selects a uniformly random node in the tree, specified using its parent such that the subtree can be replaced.
+Returns a tuple (rulenode::RuleNode, i::Int) where rulenode is the parent RuleNode and i is the index
+in rulenode.children corresponding to the selected node.
+If the root node is selected, rulenode is the root node and i is 0.
+"""
+function StatsBase.sample(::Type{NodeLoc}, root::RuleNode)
+    i = 1
+    selected = NodeLoc(root, 0)
+    stack = RuleNode[root]
+    while !isempty(stack)
+        node = pop!(stack)
+        for (j,child) in enumerate(node.children)
+            i += 1
+            if rand() ≤ 1/i
+                selected = NodeLoc(node, j)
+            end
+            append!(stack, child.children)
+        end
+    end
+    return selected
+end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,39 @@
+using ExprRules
+using Base.Test
+
+#=
+TESTS NEED TO BE MORE THOROUGHLY DEFINED
+=#
+
+srand(0)
+
+ruleset = @ruleset begin
+    Real = Base.rand()
+    Real = x
+    Real = Real + Real
+    Real = 3.0
+    Real = 1.0
+    Real = sqrt(Real)
+    Bool = Real > Real
+    Bool = Real < Real
+    Bool = true
+    Real = Bool ? Real : Real
+    Real = 2 / (1 + x)
+    Real = 2 / (1 + Real)
+end
+
+rulenode = RuleNode(3, [RuleNode(2), RuleNode(1)])
+@test string(get_executable(rulenode, ruleset)) == string(:(x + Base.rand()))
+
+rulenode = RuleNode(12, [RuleNode(3, [RuleNode(2), RuleNode(4)])])
+@test string(get_executable(rulenode, ruleset)) == string(:(2 / (1 + (x + 3.0))))
+
+rulenode = rand(RuleNode, ruleset, :Real, 3)
+
+x = 3
+eval(rulenode, ruleset)
+
+sample(rulenode)
+sample(rulenode, :Real, ruleset)
+sample(NodeLoc, rulenode)
+insert!(rulenode, sample(NodeLoc, rulenode), rand(RuleNode, ruleset, :Real, 3))
