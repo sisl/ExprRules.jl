01635d6263ae3cb601e8c768c1c7cad2611ae9e3
Ritchie Lee <ritchie.lee@gmail.com>
2018-09-27 13:30:01 -0700

    getfield should be getproperty

--- src/interpreter.jl
+++ src/interpreter.jl
@@ -14,7 +14,7 @@ export SymbolTable, interpret
 const SymbolTable = Dict{Symbol,Any}
 
 interpret(tab::SymbolTable, x::Any) = x
-interpret(tab::SymbolTable, s::Symbol) = haskey(tab,s) ? tab[s] : getfield(Main, s)
+interpret(tab::SymbolTable, s::Symbol) = haskey(tab,s) ? tab[s] : getproperty(Main, s)
 
 function interpret(tab::SymbolTable, ex::Expr)
     result = if ex.head == :call
